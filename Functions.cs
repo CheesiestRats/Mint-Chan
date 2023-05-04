1c7f1554a8596271b676faf9393694bcabc51e02
Blinky <captinblinky@gmail.com>
2023-05-04 17:30:02 -0400

    big adjustments while also syncing with DeSinc's changes.

--- Functions.cs
+++ Functions.cs
@@ -1,63 +1,28 @@
-﻿using Discord.WebSocket;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
+﻿using System;
+using System.IO;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+// regex
+using System.Text.RegularExpressions;
+
+// Stable Diffusion required json and image manipulation packages
+using Newtonsoft.Json.Linq;
+
+using RestSharp;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Png;
 namespace Mint_Chan
 {
     class Functions
     {
-        internal static string pingAndChannelTagDetectFilterRegexStr = @"<[@#]\d{15,}>";
-        public static string FilterPingsAndChannelTags(string inputMsg)
-        {
-            Regex pingAndChannelTagDetectionRegex = new Regex(pingAndChannelTagDetectFilterRegexStr);
-            // Replace pings and channel tags with their actual names
-            var matches = pingAndChannelTagDetectionRegex.Matches(inputMsg);
-
-            var uniqueMatches = matches.OfType<Match>().Select(m => m.Value).Distinct().ToList();
-
-            foreach(var match in uniqueMatches)
-            {
-                string matchedTag = match;
-                ulong matchedId = ulong.Parse(Regex.Match(matchedTag, @"\d+").Value);
-
-                SocketGuildUser matchedUser;
-                SocketGuildChannel matchedChannel;
-
-                if (matchedTag.Contains("@"))
-                {
-                    try
-                    {
-                        matchedUser = GlobalConfig.Server.GetUser(matchedId);
-                        inputMsg = inputMsg.Replace(matchedTag, $"@{matchedUser.Username}#{matchedUser.Discriminator}");
-                    }
-                    catch
-                    {
-                        break; // Not a real ID, break here.
-                    }
-                }else if (matchedTag.Contains("#"))
-                {
-                    try
-                    {
-                        matchedChannel = GlobalConfig.Server.GetChannel(matchedId);
-                        inputMsg = inputMsg.Replace(matchedTag, $"#{matchedChannel.Name}");
-                    }
-                    catch
-                    {
-                        break; // Not a real ID, break here.
-                    }
-                }
-                else
-                {
-                    break; // somehow escaped this function
-                }
-            }
-            return inputMsg;
-        }
-
+        // here is the default URL for stable diffusion web ui with --API param enabled in the launch parameters
+        public static string stableDiffUrl = "http://127.0.0.1:7860";
+        public static string imgFormatString = string.Empty;
         public static string IsSimilarToBannedWords(string input, List<string> bannedWords)
         {
             int threshold = 0;
@@ -138,6 +103,201 @@ namespace Mint_Chan
             if (isSymbolOrNumberA || isSymbolOrNumberB) return 2;
 
             return 1;
+        }
+
+        //private static async Task OobaboogaTextRequest(string parameters)
+        //{
+        //    string response = null;
+        //    // try other commonly used ports - flip flop between them with each failed attempt till it finds the right one
+        //    if (Program.oobApiEndpoint == "/api/v1/generate") // new better API, use this with the oob arg --extensions api
+        //    {
+        //        var content = new StringContent(JsonConvert.SerializeObject(parameters), Encoding.UTF8, "application/json");
+
+        //        response = await httpClient.PostAsync(Program.apiUrl, content);
+        //    }
+        //    else if (Program.oobApiEndpoint == "/run/textgen") // old default API (busted but it kinda works)
+        //    {
+        //        var payload = JsonConvert.SerializeObject(new object[] { oobaboogaInputPrompt, parameters });
+        //        var content = new StringContent(JsonConvert.SerializeObject(new { data = new[] { payload } }), Encoding.UTF8, "application/json");
+
+        //        response = await httpClient.PostAsync($"http://{Program.oobServer}:{Program.oobServerPort}/run/textgen", content); // try other commonly used port 7860
+        //    }
+        //    return response;
+        //}
+        public static async Task TakeAPic(SocketUserMessage Msg, string llmPrompt, string userPrompt)
+        {
+            var Context = new SocketCommandContext(GlobalConfig.Client, Msg);
+            var user = Context.User as SocketGuildUser;
+
+            // find the local time in japan right now to change the time of day in the selfie
+            // (you can change this to another country if you understand the code)
+            DateTime currentTime = Functions.GetCurrentTime();
+            string timeOfDayInNaturalLanguage = Functions.GetTimeOfDay(currentTime);
+            string timeOfDayStr = string.Empty;
+
+            // adds (Night) to the image prompt if it's night in japan, etc.
+            if (timeOfDayInNaturalLanguage != null)
+                timeOfDayStr = $", ({timeOfDayInNaturalLanguage})";
+            
+            if (userPrompt.Length > 4
+                && llmPrompt.Trim().Length > 2)
+            {
+                userPrompt = userPrompt.ToLower();
+
+                if (userPrompt.Contains("selfie"))
+                {
+                    imgFormatString = ", looking into the camera, "; // specifies to look into camera if you ask for a selfie
+                }
+                else if (userPrompt.Contains("person")
+                        || userPrompt.Contains("you as")
+                        || userPrompt.Contains("yourself as")
+                        || userPrompt.Contains("you cosplaying")
+                        || userPrompt.Contains("yourself cosplaying"))
+                    imgFormatString = "";   // don't say "standing next to (( A person ))" when it's just meant to be SallyBot
+                else if (userPrompt.Contains(" with a"))
+                    imgFormatString = ", she has ";
+                else if (userPrompt.Contains(" of you with "))
+                    imgFormatString = ", she is with ";
+
+                if (userPrompt.Contains("holding"))
+                {
+                    imgFormatString = imgFormatString + ", holding ";
+                }
+            }
+
+            string imgPrompt = $"1girl, solo, looking at viewer, short hair, green hair, green eyes, medium breasts{timeOfDayStr}{imgFormatString}"; // POSITIVE PROMPT - put what you want the image to look like generally. The AI will put its own prompt after this.
+            string imgNegPrompt = $"easynegative, badhandv4"; // NEGATIVE PROMPT HERE - put what you don't want to see
+
+            if (llmPrompt.Length > 0)
+            {
+                llmPrompt = $"(({llmPrompt}))"; // <-- emphasise the prompted thing in the image
+            }
+
+            //if (Msg.Author == MainGlobal.Server.Owner) // only owner
+            imgPrompt = $"{imgPrompt} {llmPrompt}";
+
+            var overrideSettings = new JObject
+            {// uncomment this line to specify a particular image model you want to use
+                //{ "sd_model_checkpoint", "AnythingV5_v5PrtRE.safetensors" }
+            };
+
+            var payload = new JObject
+            {
+                { "prompt", imgPrompt },
+                { "negative_prompt", imgNegPrompt},
+                { "steps", 20 },
+                { "width", 512 },
+                { "height", 688 },
+                { "send_images", true },
+                { "sampler_name", "DDIM" },
+                { "override_settings", overrideSettings }
+            };
+
+            // Below are all the potential json tags you can send to the stable diffusion image generator.
+            // Just add any of these you want to the payload section above.
+
+            //"enable_hr": false,
+            //"denoising_strength": 0,
+            //"firstphase_width": 0,
+            //"firstphase_height": 0,
+            //"hr_scale": 2,
+            //"hr_upscaler": "string",
+            //"hr_second_pass_steps": 0,
+            //"hr_resize_x": 0,
+            //"hr_resize_y": 0,
+            //"prompt": "",
+            //"styles": [
+            //  "string"
+            //],
+            //"seed": -1,
+            //"subseed": -1,
+            //"subseed_strength": 0,
+            //"seed_resize_from_h": -1,
+            //"seed_resize_from_w": -1,
+            //"sampler_name": "string",
+            //"batch_size": 1,
+            //"n_iter": 1,
+            //"steps": 50,
+            //"cfg_scale": 7,
+            //"width": 512,
+            //"height": 512,
+            //"restore_faces": false,
+            //"tiling": false,
+            //"do_not_save_samples": false,
+            //"do_not_save_grid": false,
+            //"negative_prompt": "string",
+            //"eta": 0,
+            //"s_churn": 0,
+            //"s_tmax": 0,
+            //"s_tmin": 0,
+            //"s_noise": 1,
+            //"override_settings": { },
+            //"override_settings_restore_afterwards": true,
+            //"script_args": [],
+            //"sampler_index": "Euler",
+            //"script_name": "string",
+            //"send_images": true,
+            //"save_images": false,
+            //"alwayson_scripts": { }
+
+            string url = $"{stableDiffUrl}/sdapi/v1/txt2img";
+            RestClient client = new RestClient();
+            RestRequest sdImgRequest = new RestRequest();
+            try
+            {
+                client = new RestClient(url);
+                sdImgRequest = new RestRequest(url, Method.Post);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"No Stable Diffusion detected at {stableDiffUrl}. Run webui-user.bat with:\n" +
+                "set COMMANDLINE_ARGS=--api\n" +
+                "in the webui-user.bat file for Automatic1111 Stable Diffusion.\n" +
+                "Stable Diffusion error message: " + ex);
+            }
+
+            sdImgRequest.AddHeader("Content-Type", "application/json");
+            sdImgRequest.AddParameter("application/json", payload.ToString(), ParameterType.RequestBody);
+            sdImgRequest.AddParameter("application/json", overrideSettings.ToString(), ParameterType.RequestBody);
+
+            var sdImgResponse = client.Execute(sdImgRequest);
+            if (sdImgResponse.IsSuccessful)
+            {
+                var jsonResponse = JObject.Parse(sdImgResponse.Content);
+                var images = jsonResponse["images"].ToObject<JArray>();
+
+                foreach (var imageBase64 in images)
+                {
+                    //string base64 = imageBase64.ToString().Split(",", 2)[1];
+                    string imageData = imageBase64.ToString();
+                    int commaIndex = imageData.IndexOf(',') + 1;
+                    string base64 = imageData.Substring(commaIndex);
+
+                    // Decode the base64 string to an image
+                    using var imageStream = new MemoryStream(Convert.FromBase64String(base64));
+                    using var image = SixLabors.ImageSharp.Image.Load(imageStream);
+
+                    // Save the image
+                    string sdImgFilePath = $"pic.png"; // put whatever file path you like here
+                    image.Save(sdImgFilePath, new PngEncoder());
+
+                    Task.Delay(1000).Wait();
+
+                    using var fileStream = new FileStream(sdImgFilePath, FileMode.Open, FileAccess.Read);
+                    var file = new FileAttachment(fileStream, "pic.png");
+                    if (Msg.Reference != null)
+                        await Context.Channel.SendFileAsync(sdImgFilePath, null, false, null, null, false, null, Msg.Reference);
+                    else
+                    {
+                        var messageReference = new MessageReference(Msg.Id);
+                        await Context.Channel.SendFileAsync(sdImgFilePath, null, false, null, null, false, null, messageReference);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Request failed: " + sdImgResponse.ErrorMessage);
+            }
         }
 
         public static DateTime GetCurrentTime()
